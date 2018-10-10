8d4357f9b9400280a5bec2d2c12f588821005f35
Alexandre Israël <bonjour@alexandre-israel.com>
2018-10-10 11:07:50 +0200

    Supplantation des paramètres XTC depuis settings.php

--- src/XtendedContent/Serve/XtcRequest/AbstractGuzzleXtcRequest.php
+++ src/XtendedContent/Serve/XtcRequest/AbstractGuzzleXtcRequest.php
@@ -9,6 +9,7 @@
 namespace Drupal\xtcguzzle\XtendedContent\Serve\XtcRequest;
 
 
+use Drupal\Core\Site\Settings;
 use Drupal\xtc\XtendedContent\API\Config;
 use Drupal\xtc\XtendedContent\Serve\XtcRequest\AbstractXtcRequest;
 use Drupal\xtcguzzle\XtendedContent\Serve\Client\GuzzleClient;
@@ -29,7 +30,14 @@ class AbstractGuzzleXtcRequest extends AbstractXtcRequest
   {
     $client = Config::getConfigs('serve', 'client');
     $xtctoken = Config::getConfigs('serve', 'xtctoken');
-    return array_merge_recursive($client, $xtctoken);
+    $params = array_merge_recursive($client, $xtctoken);
+
+    // Enable config override from settings.local.php
+    $settings = Settings::get('csoec.serve_client');
+    if(!empty($settings)){
+      return array_replace_recursive($params, $settings);
+    }
+    return $params;
   }
 
 }
