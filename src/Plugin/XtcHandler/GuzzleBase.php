68474b405c37b9e64bf207cdd4ffb4b017fa9802
Alexandre Israël <bonjour@alexandre-israel.com>
2019-01-24 15:29:13 +0100

    init Guzlle handler

---
+++ src/Plugin/XtcHandler/GuzzleBase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\xtcguzzle\Plugin\XtcHandler;
+
+
+use Drupal\xtc\PluginManager\XtcHandler\XtcHandlerPluginBase;
+
+/**
+ * Plugin implementation of the xtc_handler for File.
+ *
+ */
+abstract class GuzzleBase extends XtcHandlerPluginBase
+{
+
+}
